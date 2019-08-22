9a023ebffaa24f45a190a2c6f30f5b07a2ba9308
Victor Vuelma <victorvuelma@outlook.com>
2019-08-22 17:05:15 -0300

    add router

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 
+const routes = require('./src/routes');
+
 // Iniciando o App
 const app = express();
 
@@ -12,17 +14,7 @@ mongoose.connect(
 );
 requireDir('./src/models');
 
-const Product = mongoose.model('Product');
-
-// Primeira rota
-app.get('/', (req, res) => {
-    Product.create({
-        title: 'React Native',
-        description: 'Build native apps with React',
-        url: 'https://github.com/facebook/react-native/'
-    })
-
-    return res.send('Hello World');
-});
+//Rotas
+app.use('/api', routes);
 
 app.listen(3000);
