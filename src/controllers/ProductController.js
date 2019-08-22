4d0f416c61c5aee8360e0de207c80a8e7a3d2d06
Victor Vuelma <victorvuelma@outlook.com>
2019-08-22 17:57:40 -0300

    add update endpoint

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -19,5 +19,12 @@ module.exports = {
         const product = await Product.create(req.body);
 
         return res.json(product);
+    },
+
+    async update(req, res) {
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true});
+
+        return res.json(product);
     }
+    
 };
