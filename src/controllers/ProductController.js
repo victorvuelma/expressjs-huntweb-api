1af9cf1adfe0d5131173c3bf68f1bc6d748f1d3b
Victor Vuelma <victorvuelma@outlook.com>
2019-08-22 18:15:46 -0300

    page via query params

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -4,8 +4,9 @@ const Product = mongoose.model('Product');
 
 module.exports = {
     async index(req, res) {
+        const { page = 1 } = req.query;
         const products = await Product.paginate({ }, {
-            page: 1,
+            page,
             limit: 10
         });
 
