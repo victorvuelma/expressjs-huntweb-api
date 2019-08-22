1fa259f16f11823b030c8249a1131840eafa1698
Victor Vuelma <victorvuelma@outlook.com>
2019-08-22 17:55:21 -0300

    add show endpoint

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -9,6 +9,12 @@ module.exports = {
         return res.json(products);
     },
 
+    async show(req, res) {
+        const product = await Product.findById(req.params.id);
+
+        return res.json(product);
+    },
+
     async store(req, res) {
         const product = await Product.create(req.body);
 
