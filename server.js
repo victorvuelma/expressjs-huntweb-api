5da52bb9ca66314221a5d6b1b7a12b4ea154640b
Victor Vuelma <victorvuelma@outlook.com>
2019-08-22 17:02:32 -0300

    create mongodb connection

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 // Iniciando o App
 const app = express();
 
 // Iniciando o DB
-mongoose.connect('mongodb://localhost:27017/huntweb', { useNewUrlParser: true});
+mongoose.connect(
+    'mongodb://localhost:27017/huntweb', 
+    { useNewUrlParser: true}
+);
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
 
 // Primeira rota
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    Product.create({
+        title: 'React Native',
+        description: 'Build native apps with React',
+        url: 'https://github.com/facebook/react-native/'
+    })
+
+    return res.send('Hello World');
 });
 
 app.listen(3000);
