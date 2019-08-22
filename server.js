2b07ac90f9ea3667efb6b76f01edf1fd4abdcd88
Victor Vuelma <victorvuelma@outlook.com>
2019-08-22 16:42:04 -0300

    add mongodb connection

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+// Iniciando o App
 const app = express();
 
+// Iniciando o DB
+mongoose.connect('mongodb://localhost:27017/huntweb', { useNewUrlParser: true});
+
+// Primeira rota
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
